f03f1fceba232aa900742992ffa8e678758f3a8b
Maarksen <buch.mamo@gmail.com>
2023-02-07 13:53:17 +0100

    initial commit

---
+++ game_loader.c
@@ -0,0 +1,29 @@
+/**
+ * @brief It loads the game from a file
+ *
+ * @file game_loader.c
+ * @author Marek Buch
+ * @version 1.0
+ * @date 07-02-2023
+ * @copyright GNU Public License
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#inlclude "game_loader.h"
+
+STATUS game_create_from_file(Game *game, char *filename) {
+    if (game_create(game) == ERROR) {
+        return ERROR;
+    }
+
+    if (game_load_spaces(game, filename) == ERROR) {
+        return ERROR;
+    }
+
+    /* The player and the object are located in the first space */
+    game_set_player_location(game, game_get_space_id_at(game, 0));
+    game_set_object_location(game, game_get_space_id_at(game, 0));
+
+    return OK;
+}
---
+++ game_loader.h
@@ -0,0 +1,18 @@
+/**
+ * @brief It loads the game from a file
+ *
+ * @file game_loader.h
+ * @author Marek Buch
+ * @version 1.0
+ * @date 07-02-2023
+ * @copyright GNU Public License
+ */
+
+#ifndef SEED_SRC_GAME_LOADER_H
+#define SEED_SRC_GAME_LOADER_H
+
+STATUS game_create_from_file(Game *game, char *filename);
+
+
+
+#endif //SEED_SRC_GAME_LOADER_H
