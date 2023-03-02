c3b2723432d3b05606655bce88181a553681391a
482950 <daniel.cabrerog@estudiante.uam.es>
2023-03-02 12:26:40 +0100

    el TAD Set

---
+++ Set.c
@@ -0,0 +1,116 @@
+/*
+* @brief it implements the set module
+*
+* @file Set.c
+* @author Daniel Cabrero
+* @Version 1.0
+* @date 23-2-2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "space.h"
+#include "Set.h"
+
+/*
+* @brief Set
+*
+* This struct defines a set of entities (objects, players etc.)
+*/
+struct _Set{
+Id * ids;
+Id n_ids;
+};
+
+
+ Set * Set_create(){
+    Set *newSet;
+
+  
+  newSet = (Set *) malloc(sizeof (Set));
+  if (newSet == NULL) {
+    return NULL;
+  }
+
+  
+  newSet->ids[0] ='\0' ;
+  newSet->n_ids = 0;
+  
+
+  return newSet;
+}
+
+STATUS Set_destroy(Set *set){
+  if (!set) {
+    return ERROR;
+  }
+
+  free(set);
+  set = NULL;
+  return OK;
+}
+
+STATUS Set_Add(Set *set, Id newId){
+if(!set||set->ids[set->n_ids]=='\0'){
+  return ERROR;
+}
+set->ids[set->n_ids]=newId;
+set->n_ids++;
+
+return OK;
+
+}
+
+STATUS Set_Del(Set *set){
+if(!set){
+  return ERROR;
+}
+set->ids[set->n_ids]=0;
+set->n_ids--;
+return OK;
+
+}
+
+STATUS Set_print(Set * set){
+int i=0;
+if (!set){
+  return ERROR;
+}
+
+while(set->ids[i] !='\0'){
+  printf(" id number %d: %ld",(i+1),set->ids[i]);
+  i++;
+}
+if(i!=set->n_ids){
+  return ERROR;
+}
+  return OK;
+}
+
+BOOL Set_isEmpty(Set * set){
+if (!set||set->ids[set->n_ids]=='\0'){
+  return TRUE;
+}else{
+  return FALSE;
+}
+
+}
+
+Id Set_getId(Set * set, int pos){
+  if(!set||pos<0){
+    return -1;
+  }
+
+  return set->ids[pos];
+}
+
+STATUS Set_setId(Set * set, int pos, Id id){
+if(!set||pos<0||id<=0||set->ids[pos]!=0){
+  return ERROR;
+}
+set->ids[pos]=id;
+return OK;
+}
+
+
---
+++ Set.h
@@ -0,0 +1,94 @@
+/*
+* @brief it defines the set interface
+*
+* @file Set.c
+* @author Daniel Cabrero
+* @Version 1.0
+* @date 23-2-2023
+*/
+
+#ifndef SET_H
+#define SET_H
+
+#include "space.h"
+
+typedef struct _Set Set;
+
+/*
+* @brief it allocates memory for a new Set
+* and initializes its members
+* @author Daniel Cabrero
+*
+* @param none
+* @return an initialized Set
+*/
+Set *Set_create();
+
+/*
+* @brief it frees the memory of a set
+* @author Daniel Cabrero
+*
+* @param set a pointer to the set that must be destroyed
+* @return OK if freed correctly, ERROR otherwise
+*/
+STATUS Set_destroy(Set *set);
+
+/*
+*@brief it adds a new Id at the end of the set
+*@author Daniel Cabrero
+*
+*@param set a pointer to the set you want to add an id to
+*newId the Id that will be added
+*@return OK if added correctly, ERROR otherwhise
+*/
+STATUS Set_Add(Set *set, Id newId);
+
+/*
+*@brief it deletes the last id of the set
+*@author Daniel Cabrero
+*
+*@param set a pointer to the set you wanto to delete the last id from
+*@return OK if deleted correctly, ERROR otherwhise
+*/
+STATUS Set_Del(Set *set);
+
+/*
+*@brief it prints a set to be debugged
+*@author Daniel Cabrero
+*
+*@param set the set you want to print
+*@return OK if printed correctly, ERROR otherwhise
+*/
+STATUS Set_print(Set * set);
+
+/*
+*@brief it checks if the set is empty
+*@author Daniel Cabrero
+*
+*@param set the set you want to check
+*@return TRUE if set is empty, FALSE otherwhise
+*/
+BOOL Set_isEmpty(Set * set);
+
+/*
+*@brief returns the Id of a given position
+*@author Daniel Cabrero
+*
+*@param set the source set where you want to take the Id from
+*       pos the position of the Id
+*@return the id if it worked correctly, returns -1 otherwise
+*/
+
+Id Set_getId(Set * set, int pos);
+
+/*
+*@brief inserts an Id at a determined position of a set (it can also be used to delet an Id of a determined position)
+*@author Daniel Cabrero
+*
+*@param set pointer to the source set where you want to insert the Id in
+*       pos the position of the Id
+*       id the new Id you want to insert
+*@return Ok if the id was inserted correctly, ERROR otherwise
+*/
+STATUS Set_setId(Set * set, int pos, Id id);
+#endif
